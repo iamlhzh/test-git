e163bd38cea50f1e1981f3a198e95ff7424d4b5e
hongzheng.lu <hongzheng.lu@valueonline.cn>
2022-09-30 14:00:26 +0800

    111

---
+++ PushUtil - 副本 (3).java
@@ -0,0 +1,170 @@
+package utils;
+
+import cn.jiguang.common.ClientConfig;
+import cn.jiguang.common.DeviceType;
+import cn.jiguang.common.resp.APIConnectionException;
+import cn.jiguang.common.resp.APIRequestException;
+import cn.jpush.api.JPushClient;
+import cn.jpush.api.push.PushResult;
+import cn.jpush.api.push.model.*;
+import cn.jpush.api.push.model.audience.Audience;
+import cn.jpush.api.push.model.audience.AudienceTarget;
+import cn.jpush.api.push.model.notification.IosNotification;
+import cn.jpush.api.push.model.notification.Notification;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class PushUtil {
+
+    private static final String MASTER_SECRET = "f998b14b3df6fb2b2a2883c5";
+    private static final String APP_KEY = "b3af5764fb64cab088867e2b";
+
+    private static final String ALERT = "ALERT";
+    private static final String TITLE = "TITLE";
+    private static final String MSG_CONTENT = "MSG_CONTENT";
+
+    private static final Logger logger = LoggerFactory.getLogger(PushUtil.class);
+
+    public static void pushMsg(){
+        logger.info("进来了");
+        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY, null, ClientConfig.getInstance());
+
+        // For push, all you need do is to build PushPayload object.
+//        PushPayload payload = buildPushObject_all_all_alert();
+        //
+        Platform platform = Platform.newBuilder().addDeviceType(DeviceType.Android).build();
+        Audience audience = Audience.registrationId("1104a8979204886eb2f");
+        Notification notification = Notification.newBuilder().setAlert("我的消息！").build();
+        Message message = Message.newBuilder().setMsgContent("messageContent").setTitle("msgTitle").build();
+        Options options = null;
+        SMS sms = null;
+        String target = null;
+        Map<String, JsonObject> custom = null;
+        PushPayload payload = PushPayload.newBuilder().setPlatform(platform).setAudience(audience).setNotification(notification).setMessage(message).build();
+        try {
+            PushResult result = jpushClient.sendPush(payload);
+            logger.info("Got result - " + result);
+
+        } catch (APIConnectionException e) {
+            // Connection error, should retry later
+            logger.error("Connection error, should retry later", e);
+
+        } catch (APIRequestException e) {
+            // Should review the error, and fix the request
+            logger.error("Should review the error, and fix the request", e);
+            logger.info("HTTP Status: " + e.getStatus());
+            logger.info("Error Code: " + e.getErrorCode());
+            logger.info("Error Message: " + e.getErrorMessage());
+        }
+    }
+
+
+    /**
+     * 快捷地构建推送对象：所有平台，所有设备，内容为 ALERT 的通知。
+     * @return
+     */
+    public static PushPayload buildPushObject_all_all_alert() {
+        return PushPayload.alertAll(ALERT);
+    }
+
+    /**
+     * 构建推送对象：所有平台，推送目标是别名为 "alias1"，通知内容为 ALERT。
+     * @return
+     */
+    public static PushPayload buildPushObject_all_alias_alert() {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.alias("alias1"))
+                .setNotification(Notification.alert(ALERT))
+                .build();
+    }
+
+    /**
+     * 构建推送对象：平台是 Android，目标是 tag 为 "tag1" 的设备，内容是 Android 通知 ALERT，并且标题为 TITLE。
+     * @return
+     */
+    public static PushPayload buildPushObject_android_tag_alertWithTitle() {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.android())
+                .setAudience(Audience.tag("tag1"))
+                .setNotification(Notification.android(ALERT, TITLE, null))
+                .build();
+    }
+
+
+    /**
+     * 构建推送对象：平台是 iOS，推送目标是 "tag1", "tag_all" 的交集，推送内容同时包括通知与消息 - 通知信息是 ALERT，角标数字为 5，通知声音为 "happy"，并且附加字段 from = "JPush"；消息内容是 MSG_CONTENT。通知是 APNs 推送通道的，消息是 JPush 应用内消息通道的。APNs 的推送环境是“生产”（如果不显式设置的话，Library 会默认指定为开发）
+     * @return
+     */
+    public static PushPayload buildPushObject_ios_tagAnd_alertWithExtrasAndMessage() {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.ios())
+                .setAudience(Audience.tag_and("tag1", "tag_all"))
+                .setNotification(Notification.newBuilder()
+                        .addPlatformNotification(IosNotification.newBuilder()
+                                .setAlert(ALERT)
+                                .setBadge(5)
+                                .setSound("happy")
+                                .addExtra("from", "JPush")
+                                .build())
+                        .build())
+                .setMessage(Message.content(MSG_CONTENT))
+                .setOptions(Options.newBuilder()
+                        .setApnsProduction(true)
+                        .build())
+                .build();
+    }
+
+    /**
+     * 构建推送对象：平台是 Andorid 与 iOS，推送目标是 （"tag1" 与 "tag2" 的并集）交（"alias1" 与 "alias2" 的并集），推送内容是 - 内容为 MSG_CONTENT 的消息，并且附加字段 from = JPush。
+     * @return
+     */
+    public static PushPayload buildPushObject_ios_audienceMore_messageWithExtras() {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.android_ios())
+                .setAudience(Audience.newBuilder()
+                        .addAudienceTarget(AudienceTarget.tag("tag1", "tag2"))
+                        .addAudienceTarget(AudienceTarget.alias("alias1", "alias2"))
+                        .build())
+                .setMessage(Message.newBuilder()
+                        .setMsgContent(MSG_CONTENT)
+                        .addExtra("from", "JPush")
+                        .build())
+                .build();
+    }
+
+    /**
+     * 构建推送对象：推送内容包含SMS信息
+     */
+    public static void testSendWithSMS() {
+        JPushClient jpushClient = new JPushClient(MASTER_SECRET, APP_KEY);
+        try {
+            SMS sms = SMS.newBuilder()
+                    .setDelayTime(1000)
+                    .setTempID(2000)
+                    .addPara("Test", 1)
+                    .build();
+            PushResult result = jpushClient.sendAndroidMessageWithAlias("Test SMS", "test sms", sms, "alias1");
+            logger.info("Got result - " + result);
+        } catch (APIConnectionException e) {
+            logger.error("Connection error. Should retry later. ", e);
+        } catch (APIRequestException e) {
+            logger.error("Error response from JPush server. Should review and fix it. ", e);
+            logger.info("HTTP Status: " + e.getStatus());
+            logger.info("Error Code: " + e.getErrorCode());
+            logger.info("Error Message: " + e.getErrorMessage());
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        PushUtil.pushMsg();
+    }
+
+}
